89b0fb9319c581bf8fa55a434302b52720b5d606
wcpun <puwach123@gmail.com>
2021-07-30 23:04:31 +0800

    add test for tire basic, find n startw

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -6,30 +6,79 @@ import (
 	"testing"
 )
 
-func TestTrie(t *testing.T) {
+func TestTrieBasic(t *testing.T) {
 	trie := trie.New()
+	trie.Insert("david")
+
+	ok := trie.Find("david")
+	misc.Equals(t, true, ok)
+	ok = trie.Find("apple")
+	misc.Equals(t, false, ok)
+
+	ok = trie.StartWith("da")
+	misc.Equals(t, true, ok)
+	ok = trie.StartWith("app")
+	misc.Equals(t, false, ok)
+}
+
+func TestTrieFind(t *testing.T) {
+	tests := map[string]struct {
+		word string
+		ok   bool
+	}{
+		"case 01": {"sam", true},
+		"case 02": {"john", true},
+		"case 03": {"dogg", true},
+		"case 04": {"dog", true},
+		"case 05": {"cat", true},
+		"case 06": {"doggy", false},
+		"case 07": {"house", false},
+		"case 08": {"", false},
+		"case 09": {"sa", false},
+	}
 	words := []string{"sam", "john", "tim", "jose", "rose",
 		"cat", "dog", "dogg", "roses"}
-	prefixs := []string{"s", "d", "jo", "dog", "rose"}
+	trie := trie.New()
 	for i := 0; i < len(words); i++ {
 		trie.Insert(words[i])
 	}
-	for i := 0; i < len(words); i++ {
-		misc.Equals(t, true, trie.Find(words[i]))
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ok := trie.Find(test.word)
+			misc.Equals(t, test.ok, ok)
+		})
 	}
-	for i := 0; i < len(words); i++ {
-		misc.Equals(t, true, trie.StartWith(words[i]))
+}
+
+func TestTrieStartWith(t *testing.T) {
+	tests := map[string]struct {
+		word string
+		ok   bool
+	}{
+		"case 01": {"dog", true},
+		"case 02": {"doggy", false},
+		"case 03": {"sa", true},
+		"case 04": {"john", true},
+		"case 05": {"dogg", true},
+		"case 06": {"rose", true},
+		"case 07": {"a", false},
+		"case 08": {"go", false},
+		"case 09": {"tam", false},
+		"case 10": {"hello", false},
+		"case 11": {"rosy", false},
 	}
-	for i := 0; i < len(prefixs); i++ {
-		misc.Equals(t, true, trie.StartWith(words[i]))
+	words := []string{"sam", "john", "tim", "jose", "rose",
+		"cat", "dog", "dogg", "roses"}
+	trie := trie.New()
+	for i := 0; i < len(words); i++ {
+		trie.Insert(words[i])
 	}
 
-	nwords := []string{"sa", "t", "", "doggy", "house"}
-	nprefixs := []string{"z", "go", "tam", "doggy"}
-	for i := 0; i < len(nwords); i++ {
-		misc.Equals(t, false, trie.Find(nwords[i]))
-	}
-	for i := 0; i < len(nprefixs); i++ {
-		misc.Equals(t, false, trie.StartWith(nprefixs[i]))
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ok := trie.StartWith(test.word)
+			misc.Equals(t, test.ok, ok)
+		})
 	}
 }
